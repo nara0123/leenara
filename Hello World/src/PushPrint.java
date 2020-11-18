f111bcf3cf860ecedfa4a6a707c5bcaecab72425
nara0123 <73506553+nara0123@users.noreply.github.com>
2020-11-18 15:57:54 +0900

    Update PushPrint.java

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -3,12 +3,9 @@ public class PushPrint {
 
 	public static void main(String[] args) {
 		System.out.println("sophomore");
-<<<<<<< HEAD
 		System.out.println("Wankwang Univ");
-=======
 		SYstem.out.println("Jeonbuk Iksan");
-		
->>>>>>> refs/remotes/origin/master
+		SYstem.out.println("fighting!");
 	}
 
 }
